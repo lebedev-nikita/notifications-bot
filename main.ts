fefd6ba101e90cb7167ed270f11063d9a7486f4f
Lebedev Nikita <lebedevna@consultant.ru>
2024-03-26 19:42:50 +0300

    Added UI

--- main.ts
+++ main.ts
@@ -1,33 +1,51 @@
-import { Hono } from "npm:hono";
+import { Hono } from "https://deno.land/x/hono@v4.1.4/mod.ts";
+import { serveStatic } from "https://deno.land/x/hono@v4.1.4/middleware.ts";
 import { Telegraf } from "npm:telegraf";
 
 const hono = new Hono()
-  .get("/", (ctx) => {
-    return ctx.html(
-      `Make POST request to /sendMessage with <code>{ "message": "test" }</code>`
-    );
-  })
   .post("/sendMessage", async (ctx) => {
-    const message = await ctx.req.json().then((body) => body.message);
+    try {
+      let message: string | null = null;
 
-    if (typeof message != "string") {
-      return new Response("typeof message != 'string'", { status: 400 });
+      switch (ctx.req.header("content-type")) {
+        case "application/json": {
+          const body = await ctx.req.json();
+          message = body.message;
+          break;
+        }
+        case "application/x-www-form-urlencoded": {
+          const body = await ctx.req.formData();
+          const msg = body.get("message");
+          if (typeof msg == "string") {
+            message = msg;
+          }
+          break;
+        }
+      }
+
+      if (typeof message != "string") {
+        return new Response("typeof message != 'string'", { status: 400 });
+      }
+
+      const TELEGRAM_TOKEN = Deno.env.get("TELEGRAM_TOKEN");
+      if (!TELEGRAM_TOKEN) {
+        return new Response("Error: TELEGRAM_TOKEN not found", { status: 500 });
+      }
+
+      const CHAT_ID = Deno.env.get("CHAT_ID");
+      if (!CHAT_ID) {
+        return new Response("Error: CHAT_ID not found", { status: 500 });
+      }
+
+      const bot = new Telegraf(TELEGRAM_TOKEN);
+
+      bot.telegram.sendMessage(CHAT_ID, message);
+      return new Response(`Message "${message}" sent successfully`);
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-
-    const TELEGRAM_TOKEN = Deno.env.get("TELEGRAM_TOKEN");
-    if (!TELEGRAM_TOKEN) {
-      return new Response("Error: TELEGRAM_TOKEN not found", { status: 500 });
-    }
-
-    const CHAT_ID = Deno.env.get("CHAT_ID");
-    if (!CHAT_ID) {
-      return new Response("Error: CHAT_ID not found", { status: 500 });
-    }
-
-    const bot = new Telegraf(TELEGRAM_TOKEN);
-
-    bot.telegram.sendMessage(CHAT_ID, message);
-    return new Response(`Message "${message}" successfully sent`);
-  });
+  })
+  .use("*", serveStatic({ root: "./static" }));
 
 Deno.serve(hono.fetch);
